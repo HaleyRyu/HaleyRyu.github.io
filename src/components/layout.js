1be4e651a3a1f52fddb20d7d0a5f9af48ce012af
DeveloperCookie <developercookie28@gmail.com>
2021-07-11 12:46:51 +0900

    Add Portfolio pages

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,84 +2,125 @@ import React from "react";
 import styled from "styled-components";
 import menuIcon from "../../asset/icons/menu_black_24dp.svg";
 
-const NavBar = styled.nav`
+const Header = styled.header`
+  @keyframes navdot {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+
+  border-bottom: none;
   position: fixed;
-  top: 32px;
-  height: 35px;
-  left: 10%;
-  right: 10%;
+  width: 100%;
   z-index: 999;
+  top: 0;
+  left: 0;
+  right: 0;
+  transition: top 0.2s ease-in-out;
+  background-color: #ffffff;
 
-  .logo {
-    float: left;
-    height: 100%;
-    width: 50%;
-    font-size: 2rem;
-    font-weight: 600;
-    user-select: none;
-    position: absolute;
-    z-index: 100;
-
-    span {
-      cursor: pointer;
-    }
-
-    a {
-      text-decoration: none;
-      color: black;
-    }
-  }
-
-  .menu {
-    width: 50%;
-    height: 100%;
-    float: left;
-    margin: 0;
+  .wrapper {
+    max-width: 1290px;
+    margin: 0 auto;
+    height: 73px;
     padding: 0;
-    font-size: 2rem;
-    position: absolute;
-    z-index: 100;
-    left: 50%;
 
-    @media only screen and (max-width: 830px) {
-      display: none;
-    }
-
-    li {
-      float: right;
-      list-style: none;
-      margin-right: 40px;
-      font-weight: 500;
-      cursor: pointer;
+    .logo {
+      font-size: 2rem;
+      font-weight: 600;
       user-select: none;
+      padding-top: 19px;
+      width: fit-content;
+      float: left;
+
+      span {
+        cursor: pointer;
+      }
+
+      a {
+        text-decoration: none;
+        color: black;
+      }
     }
 
-    a {
-      text-decoration: none;
-      color: black;
+    nav {
+      text-align: right;
+      padding: 0;
+      height: 100%;
+
+      .menu {
+        display: inline-block;
+        letter-spacing: -0.5px;
+        text-align: right;
+        height: 100%;
+        padding: 0;
+        font-size: 1.8rem;
+        margin: 0;
+        padding-top: 19px;
+
+        @media only screen and (max-width: 830px) {
+          display: none;
+        }
+
+        li {
+          float: right;
+          list-style: none;
+          margin-right: 40px;
+          font-weight: 500;
+          cursor: pointer;
+          user-select: none;
+        }
+
+        li:nth-child(1) {
+          margin-right: 0;
+        }
+
+        li a.active:before {
+          animation: navdot 0.25s ease-in-out forwards 0.25s;
+          border-radius: 50%;
+          content: "";
+          background-color: #ff5252;
+          left: 0;
+          height: 10px;
+          opacity: 0;
+          position: absolute;
+          top: 50%;
+          transform: translate(-200%, -50%);
+          width: 10px;
+        }
+
+        a {
+          text-decoration: none;
+          color: black;
+          position: relative;
+        }
+      }
     }
-  }
 
-  .menuBtnWrapper {
-    display: none;
-    width: 50%;
-    height: 100%;
-    float: left;
-    margin: 0;
-    padding: 0;
-    font-size: 2rem;
-    position: absolute;
-    z-index: 100;
-    left: 50%;
+    .menuBtnWrapper {
+      display: none;
+      width: 50%;
+      height: 100%;
+      float: left;
+      margin: 0;
+      padding: 0;
+      font-size: 2rem;
+      position: absolute;
+      z-index: 100;
+      left: 50%;
 
-    @media only screen and (max-width: 830px) {
-      display: block;
-    }
+      @media only screen and (max-width: 830px) {
+        display: block;
+      }
 
-    img {
-      float: right;
-      padding-top: 3px;
-      cursor: pointer;
+      img {
+        float: right;
+        padding-top: 3px;
+        cursor: pointer;
+      }
     }
   }
 
@@ -110,28 +151,45 @@ const Contents = styled.div`
 `;
 
 export default function Layout({ children }) {
+  const pathname = window.location.pathname;
+
   return (
     <div>
-      <NavBar>
-        <div className="logo">
-          <span>
-            <a href="./">Haley Ryu</a>
-          </span>
+      <Header>
+        <div className="wrapper">
+          <div className="logo">
+            <span>
+              <a href="./">Haley Ryu</a>
+            </span>
+          </div>
+          <nav>
+            <ul className="menu">
+              <li>
+                <a href="./">Logs</a>
+              </li>
+              <li>
+                <a
+                  href="./resume"
+                  className={pathname === "/resume" ? "active" : ""}
+                >
+                  Resume
+                </a>
+              </li>
+              <li>
+                <a
+                  href="./portfolio"
+                  className={pathname === "/portfolio" ? "active" : ""}
+                >
+                  Portfolio
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div className="menuBtnWrapper">
+            <img src={menuIcon}></img>
+          </div>
         </div>
-        <ul className="menu">
-          <li>Logs</li>
-          <li>
-            <a href="./resume">Resume</a>
-          </li>
-          <li>
-            <a href="./portfolio">Portfolio</a>
-          </li>
-        </ul>
-        <div className="menuBtnWrapper">
-          <img src={menuIcon}></img>
-        </div>
-        <div className="background"></div>
-      </NavBar>
+      </Header>
       <Contents>{children}</Contents>
     </div>
   );
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -7,29 +7,357 @@ import faceIcon from "../../asset/icons/face_black_24dp.svg";
 import agentImg from "../../asset/images/page2.jpeg";
 import translatorImg from "../../asset/images/translator.png";
 import archeryImg from "../../asset/images/archery.jpeg";
+import MessageIcon from "../../asset/icons/3d/Message_perspective_matte.png";
+import AppsIcon from "../../asset/icons/3d/Add_apps_perspective_matte.png";
+import BrowserIcon from "../../asset/icons/3d/Browser_perspective_matte.png";
+import FlashIcon from "../../asset/icons/3d/Flash_perspective_matte.png";
+import TargetIcon from "../../asset/icons/3d/Target_perspective_matte.png";
+import FAQIcon from "../../asset/icons/3d/FAQ_perspective_matte.png";
+import MarketingIcon from "../../asset/icons/3d/Marketing_perspective_matte.png";
+import BellIcon from "../../asset/icons/3d/Bell_perspective_matte.png";
+import CrownIcon from "../../asset/icons/3d/Crown_perspective_matte.png";
+import DashboardIcon from "../../asset/icons/3d/Dashboard_perspective_matte.png";
+import TrophyIcon from "../../asset/icons/3d/Trophy_perspective_matte.png";
+
+const Portfolio = () => {
+  return (
+    <Layout>
+      <Scroller>
+        <div className="bar"></div>
+        <ul className="timeline">
+          <li>Current</li>
+          <li>2020.</li>
+        </ul>
+      </Scroller>
+      <Container>
+        <div id="page1">
+          <div className="description">
+            Hello, I'm Haley Ryu. <br /> I'm a proactive and creative Frontend
+            Engineer.
+            <br />I believe in the power of communication.
+          </div>
+          <img src={selfieImg} className="selfie" />
+        </div>
+        <div className="article">
+          <h3 className="companyTitle">
+            <em>Hyundai Motor Company</em>
+            <span>Software Engineer</span>
+          </h3>
+          <BigCard>
+            <div className="title">
+              <img src={AppsIcon} className="icon" />
+              <span>AI Virtual Assistant Platform</span>
+            </div>
+            <div className="contents">
+              <ul>
+                <li>asdfasdf</li>
+                <li>asdfasdf</li>
+                <li>asdfasdf</li>
+              </ul>
+              <Image
+                bg={
+                  "https://t1.kakaocdn.net/kakaocorp/kakaocorp/admin/tech/5a8b7ef7017a00001.png?type=thumb&opt=C630x472"
+                }
+              ></Image>
+            </div>
+          </BigCard>
+          <BigCard>
+            <div className="title">
+              <img src={FlashIcon} className="icon" />
+              <span>AI Virtual Assistant Platform</span>
+            </div>
+            <div className="contents">
+              <ul>
+                <li>asdfasdf</li>
+                <li>asdfasdf</li>
+                <li>asdfasdf</li>
+              </ul>
+              <Image
+                bg={
+                  "https://t1.kakaocdn.net/kakaocorp/kakaocorp/admin/tech/5a8b7ef7017a00001.png?type=thumb&opt=C630x472"
+                }
+              ></Image>
+            </div>
+          </BigCard>
+          <BigCard>
+            <div className="title">
+              <img src={TargetIcon} className="icon" />
+              <span>AI Virtual Assistant Platform</span>
+            </div>
+            <div className="contents">
+              <ul>
+                <li>asdfasdf</li>
+                <li>asdfasdf</li>
+                <li>asdfasdf</li>
+              </ul>
+              <Image
+                bg={
+                  "https://t1.kakaocdn.net/kakaocorp/kakaocorp/admin/tech/5a8b7ef7017a00001.png?type=thumb&opt=C630x472"
+                }
+              ></Image>
+            </div>
+          </BigCard>
+          <h3 className="companyTitle">
+            <em>Kakao Corp.</em>
+            <span>Software Engineer</span>
+          </h3>
+          <BigCard>
+            <div className="title">
+              <img src={FAQIcon} className="icon" />
+              <span>AI Virtual Assistant Platform</span>
+            </div>
+            <div className="contents">
+              <ul>
+                <li>asdfasdf</li>
+                <li>asdfasdf</li>
+                <li>asdfasdf</li>
+              </ul>
+              <Image
+                bg={
+                  "https://t1.kakaocdn.net/kakaocorp/kakaocorp/admin/tech/5a8b7ef7017a00001.png?type=thumb&opt=C630x472"
+                }
+              ></Image>
+            </div>
+          </BigCard>
+          <BigCard>
+            <div className="title">
+              <img src={CrownIcon} className="icon" />
+              <span>AI Virtual Assistant Platform</span>
+            </div>
+            <div className="contents">
+              <ul>
+                <li>asdfasdf</li>
+                <li>asdfasdf</li>
+                <li>asdfasdf</li>
+              </ul>
+              <Image
+                bg={
+                  "https://t1.kakaocdn.net/kakaocorp/kakaocorp/admin/tech/5a8b7ef7017a00001.png?type=thumb&opt=C630x472"
+                }
+              ></Image>
+            </div>
+          </BigCard>
+          <BigCard>
+            <div className="title">
+              <img src={BellIcon} className="icon" />
+              <span>AI Virtual Assistant Platform</span>
+            </div>
+            <div className="contents">
+              <ul>
+                <li>asdfasdf</li>
+                <li>asdfasdf</li>
+                <li>asdfasdf</li>
+              </ul>
+              <Image
+                bg={
+                  "https://t1.kakaocdn.net/kakaocorp/kakaocorp/admin/tech/5a8b7ef7017a00001.png?type=thumb&opt=C630x472"
+                }
+              ></Image>
+            </div>
+          </BigCard>
+          <h3 className="companyTitle" style={{ marginBottom: 10 }}>
+            <img
+              src={TrophyIcon}
+              width="39"
+              height="39"
+              style={{ float: "left", marginRight: 15, paddingTop: 3 }}
+            />
+            <em>Leadership, Activities and Awards</em>
+          </h3>
+          <SmallCard>
+            <div className="card">
+              <div className="title">Got a Patent</div>
+              <div className="desc" style={{ height: 124 }}>
+                <ul>
+                  <li>
+                    Server for managing natural language processing engines
+                  </li>
+                  <li>
+                    Operate as a resource to less experienced Software
+                    Engineers, providing
+                  </li>
+                </ul>
+              </div>
+              <img
+                src={
+                  "https://t1.kakaocdn.net/kakaocorp/kakaocorp/admin/tech/5a8b7ef7017a00001.png?type=thumb&opt=C630x472"
+                }
+              />
+            </div>
+          </SmallCard>
+          <SmallCard>
+            <div className="card">
+              <div className="title">Got a Patent</div>
+              <div className="desc" style={{ height: 124 }}>
+                <ul>
+                  <li>
+                    Server for managing natural language processing engines
+                  </li>
+                  <li>
+                    Operate as a resource to less experienced Software
+                    Engineers, providing
+                  </li>
+                </ul>
+              </div>
+              <img
+                src={
+                  "https://t1.kakaocdn.net/kakaocorp/kakaocorp/admin/tech/5a8b7ef7017a00001.png?type=thumb&opt=C630x472"
+                }
+              />
+            </div>
+          </SmallCard>
+          <SmallCard>
+            <div className="card">
+              <div className="title">Got a Patent</div>
+              <div className="desc" style={{ height: 124 }}>
+                <ul>
+                  <li>
+                    Server for managing natural language processing engines
+                  </li>
+                  <li>
+                    Operate as a resource to less experienced Software
+                    Engineers, providing
+                  </li>
+                </ul>
+              </div>
+              <img
+                src={
+                  "https://t1.kakaocdn.net/kakaocorp/kakaocorp/admin/tech/5a8b7ef7017a00001.png?type=thumb&opt=C630x472"
+                }
+              />
+            </div>
+          </SmallCard>
+          <SmallCard>
+            <div className="card">
+              <div className="title">Got a Patent</div>
+              <div className="desc" style={{ height: 124 }}>
+                <ul>
+                  <li>
+                    Server for managing natural language processing engines
+                  </li>
+                  <li>
+                    Operate as a resource to less experienced Software
+                    Engineers, providing
+                  </li>
+                </ul>
+              </div>
+              <img
+                src={
+                  "https://t1.kakaocdn.net/kakaocorp/kakaocorp/admin/tech/5a8b7ef7017a00001.png?type=thumb&opt=C630x472"
+                }
+              />
+            </div>
+          </SmallCard>
+          <SmallCard>
+            <div className="card">
+              <div className="title">Got a Patent</div>
+              <div className="desc" style={{ height: 124 }}>
+                <ul>
+                  <li>
+                    Server for managing natural language processing engines
+                  </li>
+                  <li>
+                    Operate as a resource to less experienced Software
+                    Engineers, providing
+                  </li>
+                </ul>
+              </div>
+              <img
+                src={
+                  "https://t1.kakaocdn.net/kakaocorp/kakaocorp/admin/tech/5a8b7ef7017a00001.png?type=thumb&opt=C630x472"
+                }
+              />
+            </div>
+          </SmallCard>
+          <SmallCard>
+            <div className="card">
+              <div className="title">Got a Patent</div>
+              <div className="desc" style={{ height: 124 }}>
+                <ul>
+                  <li>
+                    Server for managing natural language processing engines
+                  </li>
+                  <li>
+                    Operate as a resource to less experienced Software
+                    Engineers, providing
+                  </li>
+                </ul>
+              </div>
+              <img
+                src={
+                  "https://t1.kakaocdn.net/kakaocorp/kakaocorp/admin/tech/5a8b7ef7017a00001.png?type=thumb&opt=C630x472"
+                }
+              />
+            </div>
+          </SmallCard>
+        </div>
+      </Container>
+    </Layout>
+  );
+};
 
 const Container = styled.div`
-  width: 80%;
-  padding-left: 10%;
-  padding-right: 10%;
+  //width: 80%;
+  //padding-left: 10%;
+  //padding-right: 10%;
+  font-family: "Open Sans", sans-serif;
 
   #page1 {
     padding-top: 155px;
-    height: 1100px;
+    height: 700px;
 
     .description {
-      font-size: 5vw;
-      letter-spacing: 2px;
-      width: 70%;
-      position: absolute;
+      font-size: 2rem;
+      letter-spacing: 1px;
+      width: calc(100% - 400px);
+      //position: absolute;
+      float: left;
       font-weight: 600;
-      top: 300px;
-      left: 10%;
+      //top: 300px;
+      //left: 10%;
     }
     .selfie {
-      width: 44%;
-      float: right;
-      border-radius: 48%;
+      width: 400px;
+      height: 400px;
+      object-fit: cover;
+      float: left;
+      border-radius: 50%;
+    }
+  }
+
+  .article {
+    position: relative;
+    max-width: 1296px;
+    margin: 0 auto;
+    padding-bottom: 182px;
+
+    @media screen and (max-width: 1439px) {
+      max-width: 999px;
+    }
+
+    @media screen and (max-width: 1023px) {
+      //padding-bottom: 172px;
+      max-width: 666px;
+    }
+
+    @media screen and (max-width: 767px) {
+      max-width: 364px;
+      //padding-left: 24px;
+      //padding-right: 24px;
+    }
+
+    .companyTitle {
+      font-size: 33px;
+      line-height: 46px;
+      margin-top: 49px;
+
+      em {
+        display: block;
+        font-style: normal;
+      }
+      span {
+        font-size: 24px;
+      }
     }
   }
 
@@ -80,6 +408,95 @@ const Container = styled.div`
       margin-top: 17px;
     }
   }
+`;
+
+const BigCard = styled.div`
+  border-radius: 14px;
+  background-color: #fff;
+  text-align: center;
+  box-shadow: 2px 5px 40px 0 rgb(0 0 0 / 8%);
+  box-sizing: border-box;
+  margin-bottom: 39px;
+  min-height: 400px;
+
+  .title {
+    display: flex;
+    padding: 46px 49px 0;
+
+    .icon {
+      width: 39px;
+      height: 39px;
+      margin-right: 17px;
+      float: left;
+    }
+    span {
+      float: left;
+      font-size: 24px;
+      font-weight: 700;
+    }
+  }
+
+  .contents {
+  }
+`;
+
+const SmallCard = styled.div`
+  display: inline-block;
+  margin-top: 36px;
+  padding: 0 18px;
+  vertical-align: top;
+  box-sizing: border-box;
+
+  .card {
+    min-height: 420px;
+    overflow: hidden;
+    position: relative;
+    width: 297px;
+    border-radius: 14px;
+    background-color: #ffffff;
+    box-shadow: 4px 12px 30px 6px rgb(0 0 0 / 9%);
+
+    .title {
+      padding: 25px 25px 0;
+      display: -webkit-box;
+      //overflow: hidden;
+      height: fit-content;
+      font-size: 22px;
+      line-height: 32px;
+      color: black;
+      //-webkit-box-orient: vertical;
+      //-webkit-line-clamp: 2;
+      font-weight: 600;
+    }
+
+    .desc {
+      font-size: 14px;
+      padding-top: 19px;
+      color: #000000b3;
+      padding-left: 25px;
+      padding-right: 25px;
+      ul {
+        padding: 0;
+        padding-left: 25px;
+        margin-top: 0;
+        margin-bottom: 0;
+        li:nth-child(2) {
+          margin-top: 5px;
+        }
+      }
+    }
+    img {
+      width: 100%;
+      height: 220px;
+      display: block;
+    }
+  }
+`;
+
+const Image = styled.div`
+  background: url(${(props) => props.bg});
+  width: 100%;
+  height: ${(props) => (props.height ? `${props.height}px` : "600px")};
 `;
 
 const Scroller = styled.div`
@@ -185,156 +602,5 @@ const KeySkills = styled.div`
     font-size: 19px;
   }
 `;
-
-const Portfolio = () => {
-  return (
-    <Layout>
-      <Scroller>
-        <div className="bar"></div>
-        <ul className="timeline">
-          <li>Current</li>
-          <li>2020.</li>
-        </ul>
-      </Scroller>
-      <Container>
-        <div id="page1">
-          <div className="description">
-            Hi, I'm Haley Ryu. <br /> I'm a proactive Frontend Developer.
-            <br />I believe in the power of <br />
-            communication.
-          </div>
-          <img src={selfieImg} className="selfie" />
-        </div>
-        <div id="page2">
-          <Date>2020.12 - Current</Date>
-          <Head1>AI Car Agent Builder</Head1>
-          <Company>
-            <img src={companyIcon} />
-            <span>Hyundai Motor Company</span>
-          </Company>
-          <Role>
-            <img src={faceIcon} />
-            <span>Lead Frontend Developer</span>
-          </Role>
-          <img src={agentImg} className="agentImg" />
-          <Detail>
-            <li>
-              Developed a Bot builder platform specialized in Mobility Domain
-              using React, GraphQL, Apollo Client, HTML5/CSS.
-            </li>
-            <li>
-              Developed a Bot builder platform specialized in Mobility Domain
-              using React, GraphQL, Apollo Client, HTML5/CSS.
-            </li>
-            <li>
-              Developed a Bot builder platform specialized in Mobility Domain
-              using React, GraphQL, Apollo Client, HTML5/CSS.
-            </li>
-          </Detail>
-          <KeySkills>
-            <div className="title">Key Skills</div>
-            <span>React, GraphQL, Apollo Client, TypeScript, HTML, CSS</span>
-          </KeySkills>
-        </div>
-
-        <div id="page3">
-          <Date>2020.10 - 2020.12</Date>
-          <Head1>Hyundai Translator</Head1>
-          <Company>
-            <img src={companyIcon} />
-            <span>Hyundai Motor Company</span>
-          </Company>
-          <Role>
-            <img src={faceIcon} />
-            <span>Frontend Developer</span>
-          </Role>
-          <img src={translatorImg} className="translatorImg" />
-          <Detail>
-            <li>
-              Developed a Bot builder platform specialized in Mobility Domain
-              using React, GraphQL, Apollo Client, HTML5/CSS.
-            </li>
-            <li>
-              Developed a Bot builder platform specialized in Mobility Domain
-              using React, GraphQL, Apollo Client, HTML5/CSS.
-            </li>
-            <li>
-              Developed a Bot builder platform specialized in Mobility Domain
-              using React, GraphQL, Apollo Client, HTML5/CSS.
-            </li>
-          </Detail>
-          <KeySkills>
-            <div className="title">Key Skills</div>
-            <span>React, GraphQL, Apollo Client, TypeScript, HTML, CSS</span>
-          </KeySkills>
-        </div>
-
-        <div id="page4">
-          <Date>2020.07 - 2020.09</Date>
-          <Head1>Archery AI Platform</Head1>
-          <Company>
-            <img src={companyIcon} />
-            <span>Hyundai Motor Company</span>
-          </Company>
-          <Role>
-            <img src={faceIcon} />
-            <span>Frontend Developer</span>
-          </Role>
-          <img src={archeryImg} className="archeryImg" />
-          <Detail>
-            <li>
-              Developed a Bot builder platform specialized in Mobility Domain
-              using React, GraphQL, Apollo Client, HTML5/CSS.
-            </li>
-            <li>
-              Developed a Bot builder platform specialized in Mobility Domain
-              using React, GraphQL, Apollo Client, HTML5/CSS.
-            </li>
-            <li>
-              Developed a Bot builder platform specialized in Mobility Domain
-              using React, GraphQL, Apollo Client, HTML5/CSS.
-            </li>
-          </Detail>
-          <KeySkills>
-            <div className="title">Key Skills</div>
-            <span>React, GraphQL, Apollo Client, TypeScript, HTML, CSS</span>
-          </KeySkills>
-        </div>
-
-        <div id="page5">
-          <Date>2020.04 - 2020.07</Date>
-          <Head1>AI POC, Backoffice</Head1>
-          <Company>
-            <img src={companyIcon} />
-            <span>Hyundai Motor Company</span>
-          </Company>
-          <Role>
-            <img src={faceIcon} />
-            <span>Frontend Developer</span>
-          </Role>
-          <img src={archeryImg} className="archeryImg" />
-          <Detail>
-            <li>
-              Developed a Bot builder platform specialized in Mobility Domain
-              using React, GraphQL, Apollo Client, HTML5/CSS.
-            </li>
-            <li>
-              Developed a Bot builder platform specialized in Mobility Domain
-              using React, GraphQL, Apollo Client, HTML5/CSS.
-            </li>
-            <li>
-              Developed a Bot builder platform specialized in Mobility Domain
-              using React, GraphQL, Apollo Client, HTML5/CSS.
-            </li>
-          </Detail>
-          <KeySkills>
-            <div className="title">Key Skills</div>
-            <span>React, GraphQL, Apollo Client, TypeScript, HTML, CSS</span>
-          </KeySkills>
-        </div>
-      </Container>
-    </Layout>
-  );
-};
 
 export default Portfolio;
